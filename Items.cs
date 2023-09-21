8b7694aae9a4636267f9642303f77c0d6f7ed949
Dashtiss <brandontisserand@outlook.com>
2023-09-20 22:06:51 -0500

    Add project files.

---
+++ Items.cs
@@ -0,0 +1,30 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Items.cs" company="Joker119">
+// Copyright (c) Joker119. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+#pragma warning disable SA1200
+
+using CustomItems.Items;
+
+namespace CustomItems.Configs;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+
+/// <summary>
+/// All item config settings.
+/// </summary>
+public class Items
+{
+    /// <summary>
+    /// Gets the list of grenade launchers.
+    /// </summary>
+    [Description("The list of grenade launchers.")]
+    public List<GrenadeLauncher> GrenadeLaunchers { get; private set; } = new()
+    {
+        new GrenadeLauncher(),
+    };
+}
